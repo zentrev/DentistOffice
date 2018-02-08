fcce3017ce718a37b993b6ee69751330fe13fd3b
C.T.Raines.III <c.t.raines.iii@gmail.com>
2018-02-08 00:23:43 -0700

    Clifford Raines

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -5,17 +5,19 @@ import BusinessObjects.Appointment.AppointmentImp;
 import BusinessObjects.Appointment.AppointmentList;
 import BusinessObjects.Patient.Patient;
 import BusinessObjects.Procedure.Procedure;
+import BusinessObjects.Provider.Provider;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Calendar;
 
 public class Controller {
 
     AppointmentList appointmentList;
-//    PatientList patientList;
-//    ProcedureList procedureList;
-//    ProviderList providerList;
-//    UserList userList;
+    PatientList patientList;
+    ProcedureList procedureList;
+    ProviderList providerList;
+    UserList userList;
 
     public Controller(){
         appointmentList = new AppointmentList();
@@ -37,4 +39,52 @@ public class Controller {
     public void removePatient(Patient patient){
         patientList.remove(patient);
     }
+
+    public void addProcedure(Procedure procedure){
+        procedureList.add(procedure);
+    }
+
+    public void removeProcedure(Procedure procedure){
+        procedureList.remove(procedure);
+    }
+
+    public void addProvider(Provider provider){
+        providerList.add(provider);
+    }
+
+    public void removeProvider(Provider provider){
+        providerList.remove(provider);
+    }
+
+    public void addUser(User user){
+        userList.add(user);
+    }
+
+    public void removeUser(User user){
+        userList.remove(user);
+    }
+
+
+
+
+
+//    private void saveHistory(){
+//        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(fileName))){
+//            out.writeObject(this.transactions);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    private void loadHistory()  {
+//        try(ObjectInputStream in = new ObjectInputStream(new FileInputStream(fileName))){
+//            this.transactions = (TransactionHistory) in.readObject();
+//        }catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+
 }
