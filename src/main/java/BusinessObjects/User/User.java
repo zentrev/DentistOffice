1af0c4859b1e97015ed2e429e7e3cb433cb318ef
FestiveArt <jachristensen@student.neumont.edu>
2018-02-08 00:37:53 -0700

    jason

--- src/main/java/BusinessObjects/User/User.java
+++ src/main/java/BusinessObjects/User/User.java
@@ -1,4 +1,20 @@
 package BusinessObjects.User;
 
 public interface User {
+
+    public String getUserName();
+
+    public void setUserName(String userName);
+
+    public String getPassword();
+
+    public void setPassword(String password);
+
+    public String getFirstName();
+
+    public void setFirstName(String firstName);
+
+    public String getLastName();
+
+    public void setLastName(String lastName);
 }
