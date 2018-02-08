f3a01e05af23912aca09d4ed94336e1d5f490ab9
FestiveArt <jachristensen@student.neumont.edu>
2018-02-08 00:39:25 -0700

    jason

--- src/main/java/BusinessObjects/User/Administrator.java
+++ src/main/java/BusinessObjects/User/Administrator.java
@@ -1,4 +1,10 @@
 package BusinessObjects.User;
 
-public class Administrator {
+public class Administrator extends UserImp {
+
+    public Administrator(){}
+
+    public Administrator(String userName, String password, String firstName, String lastName){
+        super(userName, password,firstName,lastName);
+    }
 }
