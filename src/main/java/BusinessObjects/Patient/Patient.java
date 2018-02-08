c22fafbbcd30e3d6eaef51f6fbf69ef589720d19
FestiveArt <jachristensen@student.neumont.edu>
2018-02-08 14:35:56 -0700

    Jason

--- src/main/java/BusinessObjects/Patient/Patient.java
+++ src/main/java/BusinessObjects/Patient/Patient.java
@@ -4,43 +4,123 @@ import BusinessObjects.Appointment.AppointmentList;
 
 public interface Patient {
 
+    /**
+     * gets the set firstName
+     * @return - the set first name
+     */
     public String getFirstName();
 
+    /**
+     * sets the first name
+     * @param firstName - sets the first name
+     */
     public void setFirstName(String firstName);
 
+    /**
+     * gets the set last name
+     * @return - gets the set last name
+     */
     public String getLastName();
 
+    /**
+     * sets the last name
+     * @param lastName - sets the last name
+     */
     public void setLastName(String lastName);
 
+    /**
+     * gets the set id
+     * @return - gets the set id
+     */
     public int getId();
 
+    /**
+     * sets the id
+     * @param id - sets the id
+     */
     public void setId(int id);
 
+    /**
+     * gets the set phone number
+     * @return - gets the set phone number
+     */
     public long getPhoneNumber();
 
+    /**
+     * sets the phone number
+     * @param phoneNumber - sets the phone number
+     */
     public void setPhoneNumber(long phoneNumber);
 
+    /**
+     * gets the set email
+     * @return - gets the set Emails Address
+     */
     public String getEmailAddress();
 
+    /**
+     * sets the emailAddress
+     * @param emailAddress - sets the emailAddress
+     */
     public void setEmailAddress(String emailAddress);
 
+    /**
+     * gets the set groupId
+     * @return - gets the set groupId
+     */
     public int getGroupId();
 
+    /**
+     * sets the groupId
+     * @param groupId - sets the GroupID
+     */
     public void setGroupId(int groupId);
 
+    /**
+     * gets the set MemberId
+     * @return - gets the set MemberID
+     */
     public int getMemberId();
 
+    /**
+     * sets the MemberId
+     * @param memberId - sets the MemberId
+     */
     public void setMemberId(int memberId);
 
+    /**
+     * gets the set PaymentCard
+     * @return - gets the set PaymentCard
+     */
     public long getPaymentCard() ;
 
+    /**
+     * set the paymentCard
+     * @param paymentCard - sets the Payment Card
+     */
     public void setPaymentCard(long paymentCard);
 
+    /**
+     * gets the set Appointments
+     * @return - gets the set appointments
+     */
     public AppointmentList getAppointments() ;
 
+    /**
+     * sets Appointments
+     * @param appointments - sets the appointments
+     */
     public void setAppointments(AppointmentList appointments);
 
+    /**
+     * gets the set Inserance
+     * @return - gets the set Inserance
+     */
     public String getInserance();
 
+    /**
+     * sets the Inserance
+     * @param inserance - sets the Inserance
+     */
     public void setInserance(String inserance);
 }
--- src/main/java/BusinessObjects/Patient/PatientImp.java
+++ src/main/java/BusinessObjects/Patient/PatientImp.java
@@ -90,85 +90,161 @@ public class PatientImp implements Patient, Serializable{
     }
 
     /**
-     *
-     * @return
+     *Gets the firstName
+     * @return - the set first Name
      */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Sets the First Name
+     * @param firstName - sets the first Name
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * gets the set last name
+     * @return - the set last name
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * sets the last name
+     * @param lastName - sets the last name
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * gets the set id
+     * @return
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * sets the id
+     * @param id - sets the id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * gets the set phone number
+     * @return - gets the set phone number
+     */
     public long getPhoneNumber() {
         return phoneNumber;
     }
 
+    /**
+     * sets the phone number
+     * @param phoneNumber - sets the phone number
+     */
     public void setPhoneNumber(long phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
+    /**
+     * gets the set EmailAddress
+     * @return - gets the set Email
+     */
     public String getEmailAddress() {
         return emailAddress;
     }
 
+    /**
+     * sets the Email
+     * @param emailAddress - sets the Email
+     */
     public void setEmailAddress(String emailAddress) {
         this.emailAddress = emailAddress;
     }
 
+    /**
+     * gets set GroupId
+     * @return - gets the set GroupId
+     */
     public int getGroupId() {
         return groupId;
     }
 
+    /**
+     * sets the GroupId
+     * @param groupId - sets the GrouopId
+     */
     public void setGroupId(int groupId) {
         this.groupId = groupId;
     }
 
+    /**
+     * gets the set MemberId
+     * @return - gets the set MemberId
+     */
     public int getMemberId() {
         return memberId;
     }
 
+    /**
+     * sets the MemberId
+     * @param memberId - set the MemberId
+     */
     public void setMemberId(int memberId) {
         this.memberId = memberId;
     }
 
+    /**
+     * gets the set PaymentCard
+     * @return
+     */
     public long getPaymentCard() {
         return paymentCard;
     }
 
+    /**
+     * sets PaymentCard
+     * @param paymentCard - sets paymentCard
+     */
     public void setPaymentCard(long paymentCard) {
         this.paymentCard = paymentCard;
     }
 
+    /**
+     * gets the set Appoinments
+     * @return
+     */
     public AppointmentList getAppointments() {
         return appointments;
     }
 
+    /**
+     * sets the Appointments
+     * @param appointments - sets the Appointments
+     */
     public void setAppointments(AppointmentList appointments) {
         this.appointments = appointments;
     }
 
+    /**
+     * gets the set inserance
+     * @return - gets the set inserance
+     */
     public String getInserance() {
         return inserance;
     }
 
+    /**
+     * sets the inserance
+     * @param inserance - sets the inserance
+     */
     public void setInserance(String inserance) {
         this.inserance = inserance;
     }
