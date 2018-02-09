81c002b27b2697b89dd2caaf3e2abdf83738acd4
FestiveArt <jachristensen@student.neumont.edu>
2018-02-08 17:31:29 -0700

    Jason

--- src/main/java/BusinessObjects/User/User.java
+++ src/main/java/BusinessObjects/User/User.java
@@ -2,21 +2,56 @@ package BusinessObjects.User;
 
 import java.io.Serializable;
 
+/**
+ * Interface the User can use
+ */
 public interface User {
 
+    /**
+     * gets the set userName
+     * @return - gets the set userName
+     */
     public String getUserName();
 
+    /**
+     * sets the userName
+     * @param userName - sets the userName
+     */
     public void setUserName(String userName);
 
+    /**
+     * gets the set password
+     * @return - the set password
+     */
     public String getPassword();
 
+    /**
+     * sets the password
+     * @param password - the set password
+     */
     public void setPassword(String password);
 
+    /**
+     * gets the set password
+     * @return - the set password
+     */
     public String getFirstName();
 
+    /**
+     * sets the firstName
+     * @param firstName - the set firstName
+     */
     public void setFirstName(String firstName);
 
+    /**
+     * gets the set lastName
+     * @return - the set lastName
+     */
     public String getLastName();
 
+    /**
+     * sets the lastName
+     * @param lastName - set lastName
+     */
     public void setLastName(String lastName);
 }
