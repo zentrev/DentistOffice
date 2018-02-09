70b626de5bef3b21483179aadf97b1f257cb0a10
FestiveArt <jachristensen@student.neumont.edu>
2018-02-08 17:17:57 -0700

    Jason

--- src/main/java/BusinessObjects/Procedure/ProcedureList.java
+++ src/main/java/BusinessObjects/Procedure/ProcedureList.java
@@ -5,6 +5,9 @@ import BusinessObjects.Provider.Provider;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * List of Procedures
+ */
 public class ProcedureList extends ArrayList<ProcedureImp> implements Serializable{
 
 }
