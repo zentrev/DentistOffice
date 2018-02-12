76eddab36cfbf20c9417e34472c2227608ed5386
C.T.Raines.III <c.t.raines.iii@gmail.com>
2018-02-12 13:51:34 -0700

    Clifford Raines

--- src/main/java/BusinessObjects/Factory.java
+++ src/main/java/BusinessObjects/Factory.java
@@ -13,19 +13,19 @@ public class Factory {
         return new ProviderImp();
     }
 
-    public static Provider getPrviderInstanceInstance(String firstName,String lastName, String title, int id){
+    public static Provider getPrviderInstance(String firstName,String lastName, String title, int id){
         return new ProviderImp(firstName,lastName, title, id);
     }
 
     public static User getStandardUserInstance(){return new StanderdUser();}
 
-    public static User getStandardUserInstanceInstance(String userName,String password, String firstName, String lastName){
+    public static User getStandardUserInstance(String userName,String password, String firstName, String lastName){
         return new Administrator(userName,password, firstName, lastName);
     }
 
     public static User getAdministratorUserInstance(){return new Administrator();}
 
-    public static User getAdministratorUserInstanceInstance(String userName,String password, String firstName, String lastName){
+    public static User getAdministratorUserInstance(String userName,String password, String firstName, String lastName){
         return new Administrator(userName,password, firstName, lastName);
     }
 
