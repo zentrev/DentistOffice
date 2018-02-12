298a2bc48fceab7ae9de3fc48ba6fd2e859d3a3f
FestiveArt <jachristensen@student.neumont.edu>
2018-02-12 14:40:36 -0700

    Jason

--- src/main/java/BusinessObjects/Factory.java
+++ src/main/java/BusinessObjects/Factory.java
@@ -1,11 +1,20 @@
 package BusinessObjects;
 
+import BusinessObjects.Appointment.Appointment;
+import BusinessObjects.Appointment.AppointmentImp;
+import BusinessObjects.Appointment.AppointmentList;
+import BusinessObjects.Patient.Patient;
+import BusinessObjects.Patient.PatientImp;
+import BusinessObjects.Procedure.Procedure;
+import BusinessObjects.Procedure.ProcedureImp;
+import BusinessObjects.Procedure.ProcedureList;
 import BusinessObjects.Provider.Provider;
 import BusinessObjects.Provider.ProviderImp;
 import BusinessObjects.User.Administrator;
 import BusinessObjects.User.StanderdUser;
 import BusinessObjects.User.User;
-import BusinessObjects.User.UserImp;
+
+import java.util.Calendar;
 
 public class Factory {
 
@@ -29,7 +38,27 @@ public class Factory {
         return new Administrator(userName,password, firstName, lastName);
     }
 
+    public static Appointment getAppointmentInstance() { return new AppointmentImp();}
 
+    public static Appointment getAppointmentInsance(Patient patient, ProcedureList procedures, Calendar date){
+        return new AppointmentImp(patient,procedures,date);
+    }
 
+    public static Patient getPatientInstance(){return new PatientImp();}
 
-}
+    public static Patient getPatienInstance(String firstName, String lastName, int id, long phoneNumber, String emailAddress, int groupId,
+                                            int memberId, AppointmentList appointments, String inserance){
+        return new PatientImp(firstName,lastName,id,phoneNumber,emailAddress,groupId,memberId,appointments,inserance);
+    }
+
+    public static Procedure getProcedureInstance() { return new ProcedureImp();}
+
+    public static Procedure getProcedureInstance(Provider provider, String procedureCode, String procedureDescription, double standardCharge, double amountCharged){
+        return new ProcedureImp(provider,procedureCode,procedureDescription,standardCharge,amountCharged);
+    }
+
+    public static Procedure getProcedureInstance(Provider provider, String procedureCode, String procedureDescription, double standardCharge){
+        return new ProcedureImp(provider,procedureCode,procedureDescription,standardCharge);
+    }
+
+    }
